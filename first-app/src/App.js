de814a739e2b3a36b187fb86a147b8f693af1bca
Ankur1326 <swamiankur46@gmail.com>
2023-08-09 20:45:17 +0530

    add first app

---
+++ first-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DogCard from "./DogCard";
+
+function App() {
+  return (
+    <div>
+      hello
+    <DogCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ first-app/src/Image.js
@@ -0,0 +1,10 @@
+import DogCard from "./DogCard";
+
+function App() {
+    return (
+        <div className="image-box">
+            <img src="{props.src" />
+        </div>
+    )
+}
+export default App;

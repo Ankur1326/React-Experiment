d18af6edc2a5fe6451e5a21b32dff84de05c4d6b
Ankur1326 <swamiankur46@gmail.com>
2023-08-10 09:32:27 +0530

    App.js

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import Button from './components/Button'
+
+function App() {
+  return (
+      <div>
+        <h1 className="heading">This is a heading</h1>
+        <Button />
+      </div>
+  )
+}
+
+export default App;

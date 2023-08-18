c7398aba810eea174b79c286a8534a2a93d97158
Ankur1326 <swamiankur46@gmail.com>
2023-08-18 23:25:31 +0530

    tic-tac-toe-by-me

---
+++ tic-tac-toe-by-me/tic-tac-toe/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Card from "./Components/Card/Card"
+
+function App() {
+  return (
+    <>
+      <Card player="O"/>
+    </>
+  )
+}
+
+export default App
---
+++ tic-tac-toe-by-me/tic-tac-toe/src/Components/Card/Card.jsx
@@ -0,0 +1,18 @@
+import Icon from "../Icon/Icon";
+
+function Card({ player }) {
+    let icon = <Icon />
+    if (player == 'X') {
+        icon = <Icon name="cross"/>
+    } else if (player == "O") {
+        icon = <Icon name="circle" />
+    }
+
+    return (
+        <div className="card">
+            {icon}
+        </div>
+    )
+}
+
+export default Card;

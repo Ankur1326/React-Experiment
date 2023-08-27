8396a44e3ebaa021c29e541b5a0bdc0bee878dca
Ankur1326 <swamiankur46@gmail.com>
2023-08-27 10:43:59 +0530

    PakemonDetails

--- part-3/POKEDEX/src/components/Pokemon/Pokemon.jsx
+++ part-3/POKEDEX/src/components/Pokemon/Pokemon.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import "./Pokemon.css";
 
 function Pokemon(props) {
     return (
         <div className="pokemon-card">
-            <div>{props.name}</div>
-            <div><img src={props.image}/></div>
+            <Link to="/pokemon/2">
+                <div>{props.name}</div>
+                <div><img src={props.image}/></div>
+            </Link>
         </div>
     )
 }

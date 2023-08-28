df9512c32fddde98edeb17df19f8d8fed92f69ad
Ankur1326 <swamiankur46@gmail.com>
2023-08-28 23:35:02 +0530

    update

--- Tech_gun/first-project/src/components/NewExpense/NewExpense.js
+++ Tech_gun/first-project/src/components/NewExpense/NewExpense.js
@@ -3,12 +3,19 @@ import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css'
 
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+
+    const saveExpemseDataHandler = (expenseData) => {
+        console.log(" inside the NewExpense.js components ", expenseData);
+        props.saveAddExpenseHandler(expenseData)
+    }
+    
+    
     return (
         <div className="new-expense">
-            <ExpenseForm />
+            <ExpenseForm onSaveExpenseData={saveExpemseDataHandler}/>
         </div>
     );
 }
 
-export default NewExpense
+export default NewExpense;

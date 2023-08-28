af2dc5e262cb0149f1fb6c6c438b7cdcea0d7d12
Ankur1326 <swamiankur46@gmail.com>
2023-08-28 23:31:24 +0530

    update App.js

--- Tech_gun/first-project/src/App.js
+++ Tech_gun/first-project/src/App.js
@@ -1,42 +1,53 @@
-import ExpenseItem from './components/Expenses/ExpenseItem'
+import { React, useState } from 'react';
+// import ExpenseItem from './components/Expenses/ExpenseItem'
 import Expense from "./components/Expenses/Expense"
 import NewExpense from './components/NewExpense/NewExpense';
 
+let DUMMY_EXPENSE = [
+  {
+    id: 'e1',
+    title: 'school Fee',
+    amount: 250,
+    date: new Date(2021, 5, 12)
+  },  
+  {
+    id: 'e3',
+    title: 'rent Fee',
+    amount: 400,
+    date: new Date(2021, 1, 6)
+  },
+  {
+    id: 'e3',
+    title: 'Food Fee',
+    amount: 100,
+    date: new Date(2021, 7, 6)
+  },
+  {
+    id: 'e4',
+    title: 'rurist Fee',
+    amount: 750,
+    date: new Date(2022, 2, 4)
+  },
+]
+
 function App() {
-  let expenses = [
-    {
-      id: 'e1',
-      title: 'school Fee',
-      amount: 250,
-      date: new Date(2021, 5, 12)
-    },  
-    {
-      id: 'e3',
-      title: 'rent Fee',
-      amount: 400,
-      date: new Date(2021, 1, 6)
-    },
-    {
-      id: 'e3',
-      title: 'Food Fee',
-      amount: 100,
-      date: new Date(2021, 7, 6)
-    },
-    {
-      id: 'e4',
-      title: 'rurist Fee',
-      amount: 750,
-      date: new Date(2022, 2, 4)
-    },
-  ]
+
+  
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSE)
+
+  const addExpenseHandler = (expense) => {
+    // console.log("inside the App.js components : ", expense);
+    const updatedExpense = [expense, ...expenses];
+    setExpenses(updatedExpense)
+  } 
  
   return (
     <div>
-      <NewExpense />
-      <Expense item={expenses} />
+      <NewExpense saveAddExpenseHandler={addExpenseHandler}/>
+      <Expense items={expenses} />
     </div>
   );
 }
 
 
-export default App;
+export default App; 

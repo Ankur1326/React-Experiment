46022a319b79ac36de283741c2e76fbe0a06d7b1
Ankur1326 <swamiankur46@gmail.com>
2023-08-28 23:33:43 +0530

    ExpenseItem.js

--- Tech_gun/first-project/src/components/Expenses/ExpenseDate.js
+++ Tech_gun/first-project/src/components/Expenses/ExpenseDate.js
@@ -1,6 +1,12 @@
-import './ExpenseItem.css'
+import React from "react";
 
 function ExpenseDate(props) {
+
+    console.log(props);
+
+    if (!props.date) {
+        return <p>No date provided</p>;
+    }
     const month = props.date.toLocaleString('en-US', {month: 'long'})
     const year = props.date.getFullYear()
     const day = props.date.toLocaleString('en-US', {day: '2-digit'})
--- Tech_gun/first-project/src/components/Expenses/ExpenseItem.js
+++ Tech_gun/first-project/src/components/Expenses/ExpenseItem.js
@@ -18,7 +18,7 @@ function ExpenseItem(props) {
         <Card className='expense-item'>
               <ExpenseDate date={props.date} />
               <div className='expense-item_description'>
-                <h2>{ title }</h2>
+                <h2>{ props.title }</h2>
                 <div className='expense-item_price'>${ props.amount }</div>
               </div>
         </Card>

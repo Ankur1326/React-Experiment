13c4e4fd13df1bc532f55ea65bbaacb8dafccf23
Ankur1326 <swamiankur46@gmail.com>
2023-09-07 22:03:28 +0530

    components and hooks

--- currencyConvertor_customHooks/src/App.jsx
+++ currencyConvertor_customHooks/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// import useCurrencyInfo from './hooks/useCurrencyinfo';
+import { InputBox } from "./components/InputBox";
+import useCurrencyInfo from './hooks/useCurrencyinfo';
 
+function App() {
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from) 
+
+  const options = Object.keys(currencyInfo)
+  
+  const swap = () => 
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='text-3xl bg-yellow-300'>Hello world</h1>
+      {/* <useCurrencyInfo /> */}
+      <InputBox />
     </>
   )
 }
 
-export default App
+export default App;

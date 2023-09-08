3126f9498ab8832061c27ff73412b40e0c3b8a1c
Ankur1326 <swamiankur46@gmail.com>
2023-09-08 09:04:27 +0530

    update App.jsx

--- currencyConvertor_customHooks/src/App.jsx
+++ currencyConvertor_customHooks/src/App.jsx
@@ -15,7 +15,12 @@ function App() {
 
   const options = Object.keys(currencyInfo)
   
-  const swap = () => 
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
   
   return (
     <>
